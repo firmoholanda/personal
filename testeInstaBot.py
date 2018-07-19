2c37aa6362fe6b408d1a6e3db25d3195c42a5cb4
firmoholanda <firmo@viize.com>
2018-07-19 18:34:56 -0300

    up

--- testeInstaBot.py
+++ testeInstaBot.py
@@ -3,7 +3,7 @@ import time
 
 from instaBot.src import InstaBot
 
-bot = InstaBot('firmoholanda', 'hollanda003#')
+bot = InstaBot('firmoholanda', 'instaBotTest1234')
 bot.get_media_id_by_tag('recife')
 #bot.get_media_id_by_tag('smartmentorbr.t3')
 #bot.get_media_id_by_tag('firmo')
