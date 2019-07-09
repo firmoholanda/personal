2e08e6f1c901e164fe735c74411cd82e5dc9717b
firmo holanda <firmo@viize.com>
2019-07-09 10:47:20 -0300

    k

--- hackerrank/countingValleys.py
+++ hackerrank/countingValleys.py
@@ -10,6 +10,24 @@ import sys
 
 # Complete the countingValleys function below.
 def countingValleys(n, s):
+    valleyArray = []
+    valleyArray.append(0)                          #fist step is at sea level
+    for i in range(n): 
+        if s[i] == "U":
+            #print(valleyArray[i])
+            valleyArray.append(valleyArray[i] + 1)
+        elif s[i] == "D":
+            valleyArray.append(valleyArray[i] - 1)
+        else:
+            print("error")
+
+    totalValley = 0
+    for i in range(len(valleyArray)-1):
+        if (valleyArray[i+1] <= -1) and (valleyArray[i] == 0):
+            totalValley += 1
+
+    return totalValley
+
 
 if __name__ == '__main__':
     #fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -17,10 +35,14 @@ if __name__ == '__main__':
     #n = int(input())
     #s = input()
 
-    n = 8
-    s = "UDDDUDUU"
+    n = 10
+    s = "DUDDDUUDUU"
+
+    #answer 2
 
     result = countingValleys(n, s)
+
+    print(result)
 
     #fptr.write(str(result) + '\n')
     #fptr.close()
---
+++ hackerrank/jumpingOnTheClouds.py
@@ -0,0 +1,48 @@
+#https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c):
+    pathList = []
+
+    #check fist cloud
+    if (c[0] == 0):
+        pathList.append(0)
+        j = 0
+    else:
+        pathList.append(1)
+        j = 1
+
+    #check the rest
+    while (j <= n-2):
+        if (c[j+2] == 0):
+            j += 2
+            pathList.append(j)
+        else:
+            j += 1
+            pathList.append(j)
+            
+    #return len(pathList)-1
+    return pathList
+
+if __name__ == '__main__':
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    #n = int(input())
+    #c = list(map(int, input().rstrip().split()))
+
+    n = 6
+    c = [0, 0, 0, 1, 0, 0]
+
+    result = jumpingOnClouds(c)
+    #print(c[4])
+    print(result)
+
+    #fptr.write(str(result) + '\n')
+    #fptr.close()
