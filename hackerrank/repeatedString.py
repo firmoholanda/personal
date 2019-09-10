840137f93f7d0af325fcaf6bbdf7a5f613141ecb
firmo holanda <firmo@viize.com>
2019-09-10 17:40:27 -0300

    Update repeatedString.py

--- hackerrank/repeatedString.py
+++ hackerrank/repeatedString.py
@@ -10,39 +10,38 @@ import sys
 # Complete the repeatedString function below.
 def repeatedString(s, n):
     
-
     lenS = len(s)
+    mod = n % lenS
     totalAinS = s.count('a')
-
     nDivLenS = n / lenS
 
     totalAsSubString = math.floor(nDivLenS) * totalAinS
-
-    remanderA = n - (lenS * math.floor(nDivLenS))
-
-  
-    
+    remanderA = s[:mod].count('a')
+ 
     return (totalAsSubString + remanderA)
-    
     
 
 if __name__ == '__main__':
 
-    #s = "gfcaaaecbg"
-    #n = 547602
-
     s = "gfcaaaecbg"
     n = 547602
+
+    #aba
+    #10
+    #7
     
     #gfcaaaecbg
     #547602
     #164280
 
+    #ceebbcb
+    #817723
+    #0
+
     #kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm
     #736778906400
     #out: 51574523448
 
-    
 
     result = repeatedString(s, n)
     print(result)
