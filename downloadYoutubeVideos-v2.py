b80cb1724ba6b0c9c18e6e3a656a24b15dcf95d5
firmo holanda <firmo@viize.com>
2019-09-13 21:18:04 -0300

    Update downloadYoutubeVideos-v2.py

--- downloadYoutubeVideos-v2.py
+++ downloadYoutubeVideos-v2.py
@@ -5,7 +5,8 @@ cmdString = 'youtube-dl -v -i ' \
             '--rate-limit 2M ' \
             '--format "bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 ' \
             '--min-views 1000 ' \
-            '--output "c:\\temp\\videos\\%(title)s.%(ext)s" ' \
+            '--max-filesize 60m ' \
+            '--output "c:\\temp\\videos\\ufofoot\\%(title)s.%(ext)s" ' \
             'ytsearch10:"ufo footage" '
 
 call(cmdString, shell=True)
